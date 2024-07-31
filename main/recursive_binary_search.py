74e325be67ecdaf82254c6ac25d329368ce7e034
Thomas Yemane <thomasyemane04@gmail.com>
2024-07-30 23:52:38 -0700

    recursive version of BS method

--- main/recursive_binary_search.py
+++ main/recursive_binary_search.py
@@ -1,1 +1,11 @@
-def
+def recursive_binary_search(list, target):
+    if len(list) == 0:
+        return False
+    else:
+        midpoint = (len(list))//2
+
+        if list[midpoint] == target:
+            return True
+        else:
+            if list[midpoint] < target:
+                return recursive_binary_search(list[midpoint+1: ])
