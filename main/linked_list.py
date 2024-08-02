d70b7ac9616558e40f3f9d7db0fb60114ec5558d
Thomas Yemane <thomasyemane04@gmail.com>
2024-08-01 23:38:24 -0700

    linked_list.py comment

--- main/linked_list.py
+++ main/linked_list.py
@@ -1,4 +1,8 @@
 class Node:
+    """
+    An object for storing a single node of a linked list.
+    Models two attributes - data and the link to the next node in the list
+    """
     data = None
     next_node = None
 
