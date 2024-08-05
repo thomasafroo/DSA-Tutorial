204d7233c63f1889e263eda75b19b24403e5b05b
Thomas Yemane <thomasyemane04@gmail.com>
2024-08-05 13:40:23 -0700

    merge sort

--- main/Data Structures/merge_sort.py
+++ main/Data Structures/merge_sort.py
@@ -1,5 +1,28 @@
 def merge_sort(list):
     """
     Sorts a list in ascending order
+    Returns a new sorted list
 
+    Divide: Find the midpoint of the list and divide into sublists
+    Conquer: Recursively sort the sublists created in the previous step
+    Combine: Merge the sorted sublists created in previous step
     """
+
+    if len(list) <= 1: # stopping condition
+        return list
+
+    left_half, right_half = split(list)
+    left = merge_sort(left_half)
+    right = merge_sort(right_half)
+
+    return merge(left, right)
+
+def split(list):
+    """
+    Divide the unsorted list at midpoint into sublists
+    Returns two sublists - left and right
+    """
+
+    mid = len(list)//2
+    left = list[:mid]
+    right = list[mid:]
