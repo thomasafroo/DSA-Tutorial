8b29647ae59e66be7f0f8e6e56b2d0be379137a4
Thomas Yemane <thomasyemane04@gmail.com>
2024-08-06 19:47:40 -0700

    linked_list_merge_sort.py

--- main/Data Structures/linked_list_merge_sort.py
+++ main/Data Structures/linked_list_merge_sort.py
@@ -15,3 +15,7 @@ def merge_sort(linked_list):
         return linked_list
 
     left_half, right_half = split(linked_list)
+    left = merge_sort(left_half)
+    right = merge_sort(right_half)
+
+    return merge(left, right)
