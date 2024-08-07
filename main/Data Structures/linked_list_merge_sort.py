371d52511bccf3d02f64f68d39eb179089bc7224
Thomas Yemane <thomasyemane04@gmail.com>
2024-08-06 19:38:07 -0700

    linked_list_merge_sort.py

--- main/Data Structures/linked_list_merge_sort.py
+++ main/Data Structures/linked_list_merge_sort.py
@@ -1,2 +1,11 @@
 from linked_list import LinkedList
 
+def merge_sort(linked_list):
+    """
+    Sorts a linked list in ascending order
+    - Recursively divide the linked list into sublists containing a single node
+    - Repeatedly merge the sublists to produce sorted sublists until one remains
+
+    Returns a sorted linked list
+
+    """
